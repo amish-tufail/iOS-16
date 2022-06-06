397426379411d5c6897219caf411faab16feeebf
Meng To <shadownessguy@gmail.com>
2022-06-06 18:09:49 -0400

    Initial Commit

---
+++ iOS16/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  iOS16
+//
+//  Created by Meng To on 2022-06-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS16/iOS16App.swift
@@ -0,0 +1,17 @@
+//
+//  iOS16App.swift
+//  iOS16
+//
+//  Created by Meng To on 2022-06-06.
+//
+
+import SwiftUI
+
+@main
+struct iOS16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
